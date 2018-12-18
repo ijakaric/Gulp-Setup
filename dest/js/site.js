d4a512ddd71c911225579c6d1617f99039953d18
Igor Jakaric <igorjakaric@Igors-MacBook-Pro.local>
2018-12-18 22:19:53 +0100

    first commit

---
+++ dest/js/site.js
@@ -0,0 +1,157 @@
+"use strict";
+
+(function () {
+	function r(e, n, t) {
+		function o(i, f) {
+			if (!n[i]) {
+				if (!e[i]) {
+					var c = "function" == typeof require && require;if (!f && c) return c(i, !0);if (u) return u(i, !0);var a = new Error("Cannot find module '" + i + "'");throw a.code = "MODULE_NOT_FOUND", a;
+				}var p = n[i] = { exports: {} };e[i][0].call(p.exports, function (r) {
+					var n = e[i][1][r];return o(n || r);
+				}, p, p.exports, r, e, n, t);
+			}return n[i].exports;
+		}for (var u = "function" == typeof require && require, i = 0; i < t.length; i++) {
+			o(t[i]);
+		}return o;
+	}return r;
+})()({ 1: [function (require, module, exports) {
+		"use strict";
+
+		module.exports = {
+			$dom: {
+				window: $('window'),
+				body: $('body')
+			},
+
+			vars: {
+				windowWidth: window.innerWidth
+			},
+
+			functions: {
+				escKey: function escKey(callback) {
+					$(document).on('keyup', function (e) {
+						if (e.keyCode === 27) {
+							callback();
+						}
+					});
+				},
+
+				clickOutsideContainer: function clickOutsideContainer(selector, container, closeBtn, callback) {
+					selector.on('mouseup', function (e) {
+						e.preventDefault();
+						if (!container.is(e.target) && container.has(e.target).length === 0 && !closeBtn.is(e.target)) {
+							callback();
+						}
+					});
+				}
+			}
+		};
+	}, {}], 2: [function (require, module, exports) {
+		"use strict";
+
+		var Global = require('./global');
+		module.exports = {
+			/*-------------------------------------------------------------------------------
+   	# Cache dom and strings
+   -------------------------------------------------------------------------------*/
+			$dom: {
+				menuBtn: $('.js-menu-btn'),
+				menuNav: $('.js-main-nav'),
+				menuHasSub: $('.menu-item-has-children')
+			},
+
+			classes: {
+				open: 'open',
+				openMenu: 'menu-open'
+			},
+
+			/*-------------------------------------------------------------------------------
+   	# Initialize
+   -------------------------------------------------------------------------------*/
+			init: function init() {
+				// get dom and strings
+				var $dom = this.$dom;
+				var classes = this.classes;
+
+				// functions
+				function closeNav() {
+					$dom.menuBtn.removeClass(classes.open);
+					$dom.menuNav.removeClass(classes.open);
+					Global.$dom.body.removeClass(classes.openMenu);
+				}
+
+				if (Global.vars.windowWidth < 768) {
+					$dom.menuHasSub.each(function (i, el) {
+						$(el).append('<span class="sub-icon font-plus-circle" data-open-sub></span>');
+					});
+				}
+
+				// bind events
+				$dom.menuBtn.on('click', function (e) {
+					e.preventDefault();
+					$dom.menuBtn.toggleClass(classes.open);
+					$dom.menuNav.toggleClass(classes.open);
+					Global.$dom.body.toggleClass(classes.openMenu);
+				});
+
+				$dom.menuNav.on('click', '[data-open-sub]', function () {
+					if (Global.vars.windowWidth < 768) {
+						$(this).siblings('.sub-menu').slideToggle();
+					}
+				});
+
+				Global.functions.clickOutsideContainer($dom.menuNav, $dom.menuNav.children('ul'), $dom.menuBtn, closeNav);
+
+				Global.functions.escKey(closeNav);
+			}
+		};
+	}, { "./global": 1 }], 3: [function (require, module, exports) {
+		"use strict";
+
+		module.exports = {
+			/*-------------------------------------------------------------------------------
+   	# Cache dom and strings
+   -------------------------------------------------------------------------------*/
+			$dom: {
+				slider: $('.js-slider')
+			},
+
+			/*-------------------------------------------------------------------------------
+   	# Initialize
+   -------------------------------------------------------------------------------*/
+			init: function init() {
+				// get dom and strings
+				var $dom = this.$dom;
+
+				// slider
+				$dom.slider.slick({
+					infinite: true,
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					speed: 1000,
+					arrows: false,
+					responsive: [{
+						breakpoint: 991,
+						settings: {
+							slidesToShow: 3
+						}
+					}, {
+						breakpoint: 767,
+						settings: {
+							slidesToShow: 2
+						}
+					}]
+				});
+			}
+		};
+	}, {}], 4: [function (require, module, exports) {
+		jQuery(function ($) {
+			// Site Menu
+			var menu = require('./_site/menu');
+			menu.init();
+
+			// Slick Slider
+			var sliders = require('./_site/sliders');
+			sliders.init();
+		});
+	}, { "./_site/menu": 2, "./_site/sliders": 3 }] }, {}, [4]);
---
+++ gulpfile.js
@@ -0,0 +1,206 @@
+//load dependecies
+var gulp             = require('gulp'),
+	connect          = require('gulp-connect'),
+	autoprefixer     = require('autoprefixer'),
+	notify           = require('gulp-notify'),
+	plumber          = require('gulp-plumber'),
+	postcss          = require('gulp-postcss'),
+	iconfont         = require('gulp-iconfont'),
+	iconfontCss      = require('gulp-iconfont-css'),
+	sourcemaps       = require('gulp-sourcemaps'),
+	svgmin           = require('gulp-svgmin');
+	gutil            = require('gulp-util'),
+	sass             = require('gulp-sass'),
+	sassLint         = require('gulp-sass-lint'),
+	path             = require('path'),
+	flexBugsFix      = require('postcss-flexbugs-fixes'),
+	concat			 = require('gulp-concat'),
+	concatCss		 = require('gulp-concat-css'),
+	cleanCss		 = require('gulp-clean-css'),
+	babel            = require('gulp-babel'),
+	browserify       = require('browserify'),
+	source           = require('vinyl-source-stream'),
+	buffer           = require('vinyl-buffer'),
+	gulpSequence     = require('gulp-sequence'),
+	clean            = require('gulp-clean'),
+	uglify			 = require('gulp-uglify');
+
+
+// build
+gulp.task('build', ['plugins-css', 'css', 'js']);
+gulp.task('build-prod', ['plugins-css', 'css-prod', 'js-prod']);
+
+//icon fonts
+gulp.task('fonticons', function() {
+	return gulp.src(['assets/svg/*.svg'])
+		.pipe(iconfontCss({
+			fontName: 'fonticons',
+			cssClass: 'font',
+			path: 'assets/config/icon-font-config.scss',
+			targetPath: '../../assets/sass/base/_icon-font.scss',
+			fontPath: 'assets/icons/'
+		}))
+		.pipe(iconfont({
+			fontName: 'fonticons', // required
+			prependUnicode: true, // recommended option
+			formats: ['woff2', 'woff', 'ttf'], // default, 'woff2' and 'svg' are available
+			normalize: true,
+			centerHorizontally: true
+		}))
+		.on('glyphs', function(glyphs, options) {
+			// CSS templating, e.g.
+			console.log(glyphs, options);
+		})
+		.pipe(gulp.dest('assets/icons/'))
+});
+
+
+//error notification settings for plumber
+var msgERROR = {
+	errorHandler: notify.onError({
+		title: 'Fix that ERROR:',
+		message: "<%= error.message %>",
+		time: 2000
+	})
+};
+
+// SVG optimization
+gulp.task('svgomg', function () {
+	return gulp.src('assets/svg/*.svg')
+		.pipe(svgmin({
+			plugins: [
+				{ removeTitle: true },
+				{ removeRasterImages: true },
+				{ sortAttrs: true }
+				//{ removeStyleElement: true }
+			]
+		}))
+		.pipe(gulp.dest('assets/svg'))
+});
+
+//styles
+gulp.task('css', function() {
+	var prefix = [
+		autoprefixer({ browsers: ['last 3 versions', 'ios >= 6'] }),
+		flexBugsFix
+	];
+	return gulp.src(['assets/sass/**/*.scss'])
+		.pipe(plumber(msgERROR))
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(postcss(prefix))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('css-prod', function() {
+	var prefix = [
+		autoprefixer({ browsers: ['last 3 versions', 'ios >= 6'] }),
+		flexBugsFix
+	];
+	return gulp.src(['assets/sass/**/*.scss'])
+		.pipe(plumber(msgERROR))
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(postcss(prefix))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('plugins-css', function() {
+	return gulp.src(['assets/css-plugins/*.css'])
+		.pipe(concatCss("plugins.min.css"))
+		.pipe(cleanCss())
+		.pipe(gulp.dest('dest/css'))
+});
+
+// sass-lint
+gulp.task('sass-lint', function () {
+	return gulp.src('assets/sass/**/*.scss')
+		.pipe(sassLint({
+			config: '.sass-lint.yml'
+		}))
+		.pipe(sassLint.format())
+		.pipe(sassLint.failOnError())
+});
+
+// script
+gulp.task('plugins-js', function() {
+	return gulp.src([
+		'assets/js/_libs/*.js',
+		'assets/js/_plugins/*.js'
+	])
+		.pipe(concat('plugins.js'))
+		.pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('site-js', function() {
+	return browserify('assets/js/site.js').bundle()
+		.pipe(source('site.js'))
+		.pipe(buffer())
+		.pipe(babel({presets: ['es2015'], compact: false}))
+		.pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('merge-js', function() {
+	return gulp.src([
+		'dist/plugins.js',
+		'dist/site.js'
+	])
+		.pipe(plumber(msgERROR))
+		.pipe(sourcemaps.init())
+		.pipe(concat('site.min.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('merge-js-prod', function() {
+	return gulp.src([
+		'dist/plugins.js',
+		'dist/site.js'
+	])
+		.pipe(plumber(msgERROR))
+		.pipe(sourcemaps.init())
+		.pipe(concat('site.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('clean-js', function () {
+	return gulp.src(['dist/plugins.js', 'dist/site.js'], {read: false})
+		.pipe(clean());
+});
+
+gulp.task('js', function(callback) {
+	gulpSequence('site-js', 'plugins-js', 'merge-js', 'clean-js')(callback)
+});
+gulp.task('js-prod', gulpSequence('site-js', 'plugins-js', 'merge-js-prod', 'clean-js'));
+
+
+gulp.task('connect', function() {
+	connect.server({
+	  root: 'dest',
+	  livereload: true
+	});
+});
+
+gulp.task('html', function () {
+	gulp.src('./dest/*.html')
+	.pipe(gulp.dest('./dest'))
+	.pipe(connect.reload());
+});
+
+//watch
+gulp.task('watch', ['html'], function() {
+	
+	//watch .scss files
+	gulp.watch('assets/sass/**/*.scss', ['css', 'sass-lint']);
+	//watch site.dev.js
+	gulp.watch('assets/js/**/*.js', ['js']);
+	//watch added or changed svg files to optimize them
+	gulp.watch('assets/svg/*.svg', ['svgomg']);
+});
+
+// default
+gulp.task('default', ['connect', 'watch']);
